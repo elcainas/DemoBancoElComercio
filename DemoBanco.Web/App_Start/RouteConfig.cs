0afa60e3319b24b2c1c2e921e4483e86ac95816b
jose luis <jose.cg@outlook.com>
2017-07-05 03:59:35 -0500

    Agregando url de los servicios al HOME

--- DemoBanco.Web/App_Start/RouteConfig.cs
+++ DemoBanco.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DemoBanco.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Banco", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
